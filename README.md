30b2a8caea376bd833155589e838787c42ec4526
yousef-alramli <ysabdys1996@gmail.com>
2021-07-06 16:27:03 +0300

    updates on the JS and HTML

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 LAB - 02
 about me 
-I build a webpage about guessing game about me it mostly depend on JS I hope you like it.
+I build a webpage about guessing game with 6 quistions about me it mostly depend on JS and it have my top 10 movies and tv shows, I hope you like it.
 
 Author: yousef-alramli
 Link https://codefellows.github.io/code-201-guide/curriculum/class-02/project_setup
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+let score = 0
 
 let name = prompt ('whts your name')
 console.log(name)
@@ -15,10 +16,12 @@ switch(favcolor){
 case'yes':
 alert ('incorrect answer ');
 console .log('incorrect');
+
 break;
 case'no':
 alert('thats correct');
 console .log('correct');
+score++;
 break;
 default:
     alert('pleas answer whit yes or no only')
@@ -33,6 +36,7 @@ switch(favsport){
 case'yes':
 alert ('thats correct');
 console.log('correct');
+score++;
 break;
 case'no':
 alert('incorrect answer');
@@ -51,6 +55,7 @@ switch(uni){
 case'yes':
 alert ('thats correct');
 console.log('correct');
+score++;
 break;
 case'no':
 alert('incorrect answer');
@@ -69,6 +74,7 @@ switch(favtv){
 case'yes':
 alert ('yup thats correct, and it should be your favorite too :P ');
 console.log('correct');
+score++;
 break;
 case'no':
 alert('obviously incorrect answer -_- ');
@@ -79,6 +85,53 @@ default:
 
 }
 
+alert('you have 4 attempts for the next question')
+for (let i = 0; i < 4; i++) {
+
+    var num = prompt("guess a number between 1 to 10")
+ if (num > 3) {
+
+    alert("too high");
+
+}
+else if (num < 3) {
+
+    alert("too low");
+
+}
+
+else if (num = 3) {
+    alert ('correct');
+    score++;
+    break;
+}    
+
+}
+if (num!=3) {
+   alert('the correct answer is 3') 
+}
+alert('you have 6 attempts for the next question')
+
+let suit = ['tie', 'shirt', 'jacket', 'pants']
 
 
+for (let j = 0; j < 6; j++) {
+    var suitpices = prompt('the suit have 4 pices, name one of them')
 
+    suitpices=suitpices.toLowerCase()
+
+   if (suitpices == suit[0]||suitpices == suit[1]||suitpices == suit[2]||suitpices == suit[3]) {
+        alert("thants correct");
+        score++;
+
+        break;
+    }
+    else{
+        alert("incorrect answer try again");
+    }
+}
+if (suitpices == !suit[0]||suitpices == !suit[1]||suitpices == !suit[2]||suitpices == !suit[3]) {
+    alert("the right answers are tie, shirt, jacket and pants");
+}
+
+alert('your score is ' + score + ' /6')
